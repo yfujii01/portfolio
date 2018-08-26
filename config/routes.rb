f6b5845ce24de2c6a38abfeabd4821ef245fdb05
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-27 06:45:33 +0900

    ユーザー一覧追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
   resources :items
   devise_for :users
 	root 'pages#index'
   get 'pages/show'
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
