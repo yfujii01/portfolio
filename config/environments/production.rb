c5c07c6220766b34fffe1e89811b197c8f28e7a5
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-08-27 23:04:14 +0900

    setting heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # mailer setting
+  config.action_mailer.default_url_options = { :host => '${HEROKU_APP_NAME}.herokuapp.com', :protocol => 'https' }
+
 end
