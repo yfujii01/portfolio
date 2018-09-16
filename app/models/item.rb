3b5243dad84b869d095e0b5ba635a3909aba4a48
yfujii01 <33822726+yfujii01@users.noreply.github.com>
2018-09-16 14:16:10 +0900

    fix 詳細画面で改行が反映されなかった問題を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   validates :title, presence: true
+
+  # 詳細をHTML表示用に返却する
+  # DBに登録されたタグは無効化し
+  # 改行はbrタグに変換
+  def getDetailTextView
+    ERB::Util.html_escape(detail).gsub(/(\n|\r\n)/,"<br />").html_safe
+  end
 end
